9f0b772a5610606793de23f9e4f83eaeec7d7526
Jonathan Buzzetti <jonathan.buzzetti@naranjax.com>
2021-07-13 23:49:09 -0300

    formato de cron

--- quartz-demo/src/main/java/com/example/demoquartz/SchedulerWithCronTriggerExample.java
+++ quartz-demo/src/main/java/com/example/demoquartz/SchedulerWithCronTriggerExample.java
@@ -48,7 +48,17 @@ public class SchedulerWithCronTriggerExample implements ICuentaRegresiva {
         System.out.println("Hora actual: " + new Date());
 
         // Construccion de Trigger
-        // Corre todos los dias a la hora actual mas un minuto
+
+        // Formato de expresion cron
+        // ┌───────────── minute (0 - 59)
+        // │ ┌───────────── hour (0 - 23)
+        // │ │ ┌───────────── day of the month (1 - 31)
+        // │ │ │ ┌───────────── month (1 - 12)
+        // │ │ │ │ ┌───────────── day of the week (0 - 6) (Sunday to Saturday)
+        // │ │ │ │ │ ┌───────────── year
+        // │ │ │ │ │ │
+        // * * * * * *
+
         String cron = "0 " + (min + 1) + " " + hour + " * * ? *";
         Trigger trigger = TriggerBuilder.newTrigger()
                 .withIdentity("unTrigger")
